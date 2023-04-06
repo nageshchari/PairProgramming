54437e87af68c68f48ac4246cc45e5c2817e3a3d
2255 nagesh <80745867+nageshchari@users.noreply.github.com>
2023-04-06 15:54:21 +0530

    Add files via upload

---
+++ AgileAssignment/src/Money.java
@@ -0,0 +1,20 @@
+
+
+public class Money {
+	private int moneyValue;
+	private String moneyCurrency;
+	
+	public Money(int moneyValue, String moneyCurrency ) {
+		this.moneyValue = moneyValue;
+		this.moneyCurrency = moneyCurrency;
+	}
+    
+	public int getMoneyValue() {
+		return this.moneyValue;
+	}
+	
+	public String getMoneyCurrency() {
+		return moneyCurrency;
+	}
+}
+
---
+++ AgileAssignment/src/MoneyTest.java
@@ -0,0 +1,23 @@
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MoneyTest {Money myMoney = new Money(200, "Rs");
+Money myMoney2 = new Money(200, "$");
+
+@Test
+	void testMoneyValue() {
+		//fail("Not yet implemented");
+		assertEquals(myMoney.getMoneyValue(),200);
+	}
+	
+
+	@Test
+	void testCurrencyValue() {
+		//fail("Not yet implemented");
+		assertEquals(myMoney.getMoneyCurrency(),"Rs");
+	}
+
+}
